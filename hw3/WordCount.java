05dac70f7af2db09add1500ab52a0ab482c4e862
Matthew Apuya <m.apuya19@gmail.com>
2022-09-26 04:46:35 -0400

    restructure and finish part 2

--- hw3/WordCount.java
+++ hw3/WordCount.java
@@ -1,52 +1,30 @@
-package hw3;
+import java.io.IOException;
+// import java.util.StringTokenizer;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-
-/**
- * WordCount
- */
 public class WordCount {
-    public static void main(String[] args) {
-        String[] words = {"hackathon", "dec", "chicago", "java", "engineers" };
-        int hackMatch = 0;
-        int decMatch = 0;
-        int chiMatch = 0;
-        int jaMatch = 0;
-        int engMatch = 0;
-
-        try {
-            File file = new File("input_hw_3.txt");
-            Scanner read = new Scanner(file);
-
-            while (read.hasNextLine()) {
-                String data = read.nextLine();
-                System.out.println(data);
-                for (int i = 0; i <= words.length - 1; i++) {
-                    String wordCheck = words[i];
-                    if (data.toLowerCase().contains(wordCheck)) {
-                        switch (words[i]) {
-                            case "hackathon" -> hackMatch++;
-                            case "dec" -> decMatch++;
-                            case "chicago" -> chiMatch++;
-                            case "java" -> jaMatch++;
-                            case "engineers" -> engMatch++;
-                        }
-                    }
-                }
-            }
-
-            System.out.println("Chicago " + chiMatch);
-            System.out.println("Dec " + decMatch);
-            System.out.println("Java " + jaMatch);
-            System.out.println("Hackathon " + hackMatch);
-            System.out.println("Engineers " + engMatch);
-
-            read.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("An error occured.");
-            e.printStackTrace();
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Usage: WordCount <input path> <output path>");
+            System.exit(-1);
         }
+        
+        Job job = new Job();
+        job.setJarByClass(WordCount.class);
+        job.setJobName("Word Count");
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        job.setMapperClass(WordCountMapper.class);
+        job.setReducerClass(WordCountReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        job.setNumReduceTasks(1); // 1 Reduce task
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
---
+++ hw3/WordCountReducer.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Reducer.Context;
+
+public class WordCountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+    public void reduce(Text key, Iterable<IntWritable> values, Context context)
+            throws IOException, InterruptedException {
+        int sum = 0;
+
+        for (IntWritable val : values) {
+            sum += val.get();
+        }
+        
+        String keyToString = key.toString();
+        String newKey = keyToString.substring(0, 1).toUpperCase() + keyToString.substring(1);
+        
+        context.write(new Text(newKey), new IntWritable(sum));
+    }
+}
